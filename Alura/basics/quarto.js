cf6498232e4adacc86db92dfbc3dafb981b1cd04
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-03 12:10:58 -0300

    feat: JS classes

---
+++ Alura/basics/quarto.js
@@ -0,0 +1,8 @@
+console.log("Conversão de Tipos")
+
+console.log("ano" + 2024) //string + number
+console.log("2" + "2") //string + string
+console.log(parseInt("2") + parseInt("2"))
+
+console.log("10" / "2") //Mesmo assim ele divide
+console.log("Vinicius" / "2") // NaN
---
+++ Alura/basics/segundo.js
@@ -0,0 +1,6 @@
+console.log("Olá")
+//JS é case sensitive
+
+idade = 29
+
+console.log(Idade)
