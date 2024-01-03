45644c067d28d132a7350b3c9246d07eed2e6a2b
Vinicius Duarte <vsduartestudy@outlook.com>
2024-01-03 13:01:24 -0300

    feat: more classes

--- Alura/basics/quarto.js
+++ Alura/basics/quarto.js
@@ -6,3 +6,6 @@ console.log(parseInt("2") + parseInt("2"))
 
 console.log("10" / "2") //Mesmo assim ele divide
 console.log("Vinicius" / "2") // NaN
+
+console.log(6.5)
+console.log(6,5)
---
+++ Alura/basics/quinto.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com Atribuição de Variáveis")
+const idade = 25
+const nome = "Vinicius"
+const sobrenome = "Duarte"
+
+console.log(nome, sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos!`)
