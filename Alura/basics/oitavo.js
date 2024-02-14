7ff0b0ef074f463975d9a0e3c43934d0fb75ed3a
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:02 -0300

    update ; in old files

--- Alura/basics/oitavo.js
+++ Alura/basics/oitavo.js
@@ -1,40 +1,40 @@
-console.log('Trabalhando com Condicionais')
+console.log('Trabalhando com Condicionais');
 const listaDeDestinos = new Array(
     'Salvador',
     'São Paulo',
     'Rio de Janeiro'
-)
-let comprador = 17
-let acompanhada = false
-let passagem = false
-let destino = 'Salvador'
-let destinoExiste = false
+);
+let comprador = 17;
+let acompanhada = false;
+let passagem = false;
+let destino = 'Salvador';
+let destinoExiste = false;
 
 
-const podeComprar = comprador >= 18 || acompanhada == true
+const podeComprar = comprador >= 18 || acompanhada == true;
 
-let contador = 0
+let contador = 0;
 while(contador < 3){
     if(listaDeDestinos[contador] == destino){
-        console.log('Destino Existente')
-        destinoExiste = true
-        break
-    }
-    contador += 1
-}
+        console.log('Destino Existente');
+        destinoExiste = true;
+        break;
+    };
+    contador += 1;
+};
 
-console.log('Destino Existe: ', destinoExiste)
+console.log('Destino Existe: ', destinoExiste);
 
 if(podeComprar && destinoExiste) {
-    console.log('Boa Viagem!!')
+    console.log('Boa Viagem!!');
 } else {
-    console.log('ERRO')
-}
+    console.log('ERRO');
+};
 
 for(let cont; cont < 3; cont++){
     if(listaDeDestinos[contador] == destino){
-        console.log('Destino Existente')
-        destinoExiste = true
-        break
-    }
-}
+        console.log('Destino Existente');
+        destinoExiste = true;
+        break;
+    };
+};
