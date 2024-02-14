e753d64caee9b19b1d9575d27dbc6a48916351f4
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:09 -0300

    update ; in old files

--- Alura/basics/quinto.js
+++ Alura/basics/quinto.js
@@ -1,13 +1,13 @@
-console.log("Trabalhando com Atribuição de Variáveis")
-const idade = 25
-const nome = "Vinicius"
-const sobrenome = "Duarte"
+console.log("Trabalhando com Atribuição de Variáveis");
+const idade = 25;
+const nome = "Vinicius";
+const sobrenome = "Duarte";
 
-console.log(nome, sobrenome)
-console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos!`)
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos!`);
 
-const nomeCompleto = nome + sobrenome
-console.log(nomeCompleto)
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
 
-let contador = 0
-contador = contador + 1
+let contador = 0;
+contador = contador + 1;
