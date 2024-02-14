1bd771f64e4a43512189191ad6d544c5ca61020d
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:59:40 -0300

    update ; in old files

--- CursoEmVideo/aula11/ex11.js
+++ CursoEmVideo/aula11/ex11.js
@@ -1,10 +1,10 @@
 function verify() {
-    var pais = window.document.querySelector('input#txtt').value
-    var res = window.document.querySelector('div#res')
+    var pais = window.document.querySelector('input#txtt').value;
+    var res = window.document.querySelector('div#res');
     
     if(pais === 'Brasil') {
-        res.innerHTML = 'Você é brasileiro!'
+        res.innerHTML = 'Você é brasileiro!';
     } else {
-        res.innerHTML = `Estrangeiro`
-    }
-}
+        res.innerHTML = `Estrangeiro`;
+    };
+};
