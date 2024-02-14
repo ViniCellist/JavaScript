357c8f0fca6ca2600c7e8ec34642e135c2d43f9c
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:18 -0300

    update ; in old files

--- Alura/basics/terceiro.js
+++ Alura/basics/terceiro.js
@@ -1,5 +1,5 @@
-console.log("Operações Aritiméticas")
+console.log("Operações Aritiméticas");
 
-console.log(2 + 2) //soma simples
-console.log(10 + 8 * 2) //ordem de precedencia aritimética
-console.log((10 + 8) * 2) //aspas primeiro
+console.log(2 + 2); //soma simples
+console.log(10 + 8 * 2); //ordem de precedencia aritimética
+console.log((10 + 8) * 2); //aspas primeiro
