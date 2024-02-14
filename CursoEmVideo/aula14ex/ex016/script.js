2676b3659ff7f406a078aba77cfe5481ffc02e54
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:59:58 -0300

    update ; in old files

--- CursoEmVideo/aula14ex/ex016/script.js
+++ CursoEmVideo/aula14ex/ex016/script.js
@@ -1,30 +1,30 @@
 function contar() {
-    var inicio = document.getElementById('txti')
-    var fim = document.getElementById('txtf')
-    var passo = document.getElementById('txtp')
-    var res = document.getElementById('res')
-    var emo = '👉'
+    var inicio = document.getElementById('txti');
+    var fim = document.getElementById('txtf');
+    var passo = document.getElementById('txtp');
+    var res = document.getElementById('res');
+    var emo = '👉';
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossivel Contar!'
+        res.innerHTML = 'Impossivel Contar!';
     } else {
-        res.innerHTML = 'Contando: <br>'
-        var i = Number(inicio.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
+        res.innerHTML = 'Contando: <br>';
+        var i = Number(inicio.value);
+        var f = Number(fim.value);
+        var p = Number(passo.value);
         if (p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
-            p = 1
-        }
+            window.alert('Passo inválido! Considerando PASSO 1');
+            p = 1;
+        };
 
         if (i < f) {
             for(var c = i; c <= f; c+= p) {
-                res.innerHTML += ` ${c} ${emo}`
-            }
+                res.innerHTML += ` ${c} ${emo}`;
+            };
         } else {
             for(var c = i; c >= f; c-= p) {
-                res.innerHTML += ` ${c} ${emo}`
-            }
-        }
-    }
-}
+                res.innerHTML += ` ${c} ${emo}`;
+            };
+        };
+    };
+};
