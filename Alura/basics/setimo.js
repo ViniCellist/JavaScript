0cd1851d3b65dd923a6591a83a4931235d01148c
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:13 -0300

    update ; in old files

--- Alura/basics/setimo.js
+++ Alura/basics/setimo.js
@@ -1,20 +1,20 @@
-console.log('Trabalhando com Condicionais')
+console.log('Trabalhando com Condicionais');
 const listaDeDestinos = new Array(
     'Salvador',
     'São Paulo',
     'Rio de Janeiro'
-)
-let comprador = 17
-let acompanhada = false
+);
+let comprador = 17;
+let acompanhada = false;
 
 if (comprador >= 18) {
-    console.log('Destinos Possiveis:')
-    console.log(listaDeDestinos)
+    console.log('Destinos Possiveis:');
+    console.log(listaDeDestinos);
 } else {
     if (acompanhada) {
-        console.log('Destinos Possiveis:')
-        console.log(listaDeDestinos)
+        console.log('Destinos Possiveis:');
+        console.log(listaDeDestinos);
     } else {
-        console.log('Você é menor de idade e não está acompanhado(a)!')
-    }
-}
+        console.log('Você é menor de idade e não está acompanhado(a)!');
+    };
+};
