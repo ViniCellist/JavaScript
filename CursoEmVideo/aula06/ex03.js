1db829828cfcc695583aeada4e24c1fb98322774
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:59:20 -0300

    update ; in old files

--- CursoEmVideo/aula06/ex03.js
+++ CursoEmVideo/aula06/ex03.js
@@ -2,7 +2,7 @@
 // o '+' tambem pode ser lido como concatenação
 // Number.parseInt() converte para tipo inteiro
 // String(n) ou n.toString() converte para 'string'
-var n1 = Number.parseInt(window.prompt("Digite o primeiro numero"))
-var n2 = Number.parseInt(window.prompt("Digite o segundo numero"))
-var s = n1 + n2
-window.alert(`A soma dos valores é ${s}`)
+var n1 = Number.parseInt(window.prompt("Digite o primeiro numero"));
+var n2 = Number.parseInt(window.prompt("Digite o segundo numero"));
+var s = n1 + n2;
+window.alert(`A soma dos valores é ${s}`);
