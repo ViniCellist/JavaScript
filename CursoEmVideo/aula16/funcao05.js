cba03b81fa114c6a8e8a70bb9a4cc7897e6948a0
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:00:26 -0300

    update ; in old files

--- CursoEmVideo/aula16/funcao05.js
+++ CursoEmVideo/aula16/funcao05.js
@@ -1,10 +1,10 @@
 //RECURSIVIDADE
 function fatorial(n) {
     if (n ==1) {
-        return 1
+        return 1;
     } else {
-        return n * fatorial(n-1)
-    }
-}
+        return n * fatorial(n-1);
+    };
+};
 
-console.log(fatorial(5))
+console.log(fatorial(5));
