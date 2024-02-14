374d1b7d8bfc4a4882dcc8179a39394b44162b80
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:06:58 -0300

    update ; in old files

--- Alura/aluramidi/main.js
+++ Alura/aluramidi/main.js
@@ -1,30 +1,30 @@
 function Som (seletorAudio) {
-   const elemento = document.querySelector(seletorAudio)
+   const elemento = document.querySelector(seletorAudio);
    if (elemento != null && elemento.localName === 'audio'){
-        elemento.play()
+        elemento.play();
    } else {
-        window.alert('Elemento não encontrado ou Seletor inválido!')
-   }
-}
-const ListaDeTeclas = document.querySelectorAll('.tecla')
+        window.alert('Elemento não encontrado ou Seletor inválido!');
+   };
+};
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let cont = 0; cont < ListaDeTeclas.length; cont++) {
-    const tecla = ListaDeTeclas[cont]
-    const instrument = tecla.classList[1]
-    const idAudio = `#som_${instrument}`
+    const tecla = ListaDeTeclas[cont];
+    const instrument = tecla.classList[1];
+    const idAudio = `#som_${instrument}`;
 
     tecla.onclick = function () {
-        Som(idAudio)
-    }
+        Som(idAudio);
+    };
 
     tecla.onkeydown = function (evento) {
 
         if (evento.code === 'Space' || evento.code === 'NumpadEnter' || evento.code === 'Enter') {
-            tecla.classList.add('ativa')
-        }
-    }
+            tecla.classList.add('ativa');
+        };
+    };
 
     tecla.onkeyup = function () {
-        tecla.classList.remove('ativa')
-    }
-}
+        tecla.classList.remove('ativa');
+    };
+};
