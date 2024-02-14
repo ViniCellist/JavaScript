cdaeef28a7158f1d88a8bb1bbff93c8d1176f0ed
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:59:33 -0300

    update ; in old files

--- CursoEmVideo/aula11/ex008.js
+++ CursoEmVideo/aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 70
-console.log(`A velocidade do teu carro é ${vel}Km/h`)
+var vel = 70;
+console.log(`A velocidade do teu carro é ${vel}Km/h`);
 if (vel > 60) { //Condição Simples!
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-console.log('Dirija sempre usando cintos de segurança')
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!');
+};
+console.log('Dirija sempre usando cintos de segurança');
