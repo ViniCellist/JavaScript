0c2cd19a02973c6d5d7aa2cecb23c0155a241807
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:41:10 -0300

    update

---
+++ CursoEmVideo/aula06/ex03.js
@@ -0,0 +1,8 @@
+// o comando window.prompt() devolve o valor como 'string'
+// o '+' tambem pode ser lido como concatenação
+// Number.parseInt() converte para tipo inteiro
+// String(n) ou n.toString() converte para 'string'
+var n1 = Number.parseInt(window.prompt("Digite o primeiro numero"))
+var n2 = Number.parseInt(window.prompt("Digite o segundo numero"))
+var s = n1 + n2
+window.alert(`A soma dos valores é ${s}`)
