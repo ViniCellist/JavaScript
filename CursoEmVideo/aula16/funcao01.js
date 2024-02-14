f495ccb1651b7c5e1d9e3ffed2c7f2eb817696d3
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:00:13 -0300

    update ; in old files

--- CursoEmVideo/aula16/funcao01.js
+++ CursoEmVideo/aula16/funcao01.js
@@ -1,10 +1,10 @@
 function parimpar(n) {
     if (n % 2 == 0) {
-        return 'Par!'
+        return 'Par!';
         
     } else {
-        return 'Impar!'
-    }
-}
-let res = parimpar(4)
-console.log(res)
+        return 'Impar!';
+    };
+};
+let res = parimpar(4);
+console.log(res);
