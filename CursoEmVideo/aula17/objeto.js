12a713fce0583450d45c682ad816b391f64c3fcb
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:00:31 -0300

    update ; in old files

--- CursoEmVideo/aula17/objeto.js
+++ CursoEmVideo/aula17/objeto.js
@@ -1,12 +1,13 @@
 let amigo = {nome:'Vinicius', 
-sexo:'M', 
-peso: 70,
-engordar(p){
-    console.log('engordou')
-    this.peso += p
-}}
+    sexo:'M', 
+    peso: 70,
+    engordar(p){
+        console.log('engordou');
+        this.peso += p;
+    }
+};
 
-amigo.engordar(5)
-console.log(amigo.peso)
+amigo.engordar(5);
+console.log(amigo.peso);
 
 //modularização, RegEx, JSON, AJAX, NodeJS
