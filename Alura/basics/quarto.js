1800ec5832a7854fbcd41df3c985f45ddc8bc1d7
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:07 -0300

    update ; in old files

--- Alura/basics/quarto.js
+++ Alura/basics/quarto.js
@@ -1,11 +1,11 @@
-console.log("Conversão de Tipos")
+console.log("Conversão de Tipos");
 
-console.log("ano" + 2024) //string + number
-console.log("2" + "2") //string + string
-console.log(parseInt("2") + parseInt("2"))
+console.log("ano" + 2024); //string + number
+console.log("2" + "2"); //string + string
+console.log(parseInt("2") + parseInt("2"));
 
-console.log("10" / "2") //Mesmo assim ele divide
-console.log("Vinicius" / "2") // NaN
+console.log("10" / "2"); //Mesmo assim ele divide
+console.log("Vinicius" / "2"); // NaN
 
-console.log(6.5)
-console.log(6,5)
+console.log(6.5);
+console.log(6,5);
