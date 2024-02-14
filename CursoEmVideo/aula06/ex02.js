8ed7fa8f562c317b4d83ebab7240b4fb739e0423
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 17:59:17 -0300

    update ; in old files

--- CursoEmVideo/aula06/ex02.js
+++ CursoEmVideo/aula06/ex02.js
@@ -1,2 +1,2 @@
-let nome = window.prompt('Qual é seu nome?') //pergunta o nome
-window.alert("É um grande prazer te conhecer, " + nome)
+let nome = window.prompt('Qual é seu nome?'); //pergunta o nome
+window.alert("É um grande prazer te conhecer, " + nome);
