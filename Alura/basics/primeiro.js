7b3523c220b8f9aea1d684d85d074467fb6d255b
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-14 18:07:04 -0300

    update ; in old files

--- Alura/basics/primeiro.js
+++ Alura/basics/primeiro.js
@@ -1,12 +1,12 @@
-console.log('Trabalhando com variáveis')
+console.log('Trabalhando com variáveis');
 
-const idade = 26
+const idade = 26;
 
-console.log("idade")
-console.log(idade)
-console.log(idade + 2)
-console.log(idade - 2)
-console.log(idade / 2)
+console.log("idade");
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
+console.log(idade / 2);
 
 const idadeSomada = idade + 2;
-console.log(idadeSomada)
+console.log(idadeSomada);
