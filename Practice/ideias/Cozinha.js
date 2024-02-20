5031ab31d0b0f8f752c13ccc446783ba61d416b4
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-20 10:39:23 -0300

    Create Cozinha.js

---
+++ Practice/ideias/Cozinha.js
@@ -0,0 +1,10 @@
+export class Cozinha {
+    constructor(fogao, geladeira, mesa, cadeira, microondas, armario) {
+        this.fogao = fogao;
+        this.geladeira = geladeira;
+        this.mesa = mesa;
+        this.cadeira = cadeira;
+        this.microondas = microondas;
+        this.armario = armario;
+    };
+};
