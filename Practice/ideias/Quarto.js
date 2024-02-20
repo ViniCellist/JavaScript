cbfc12d24a1d19ead47e63275870f0ee64b2b35b
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-20 10:39:29 -0300

    Create Quarto.js

---
+++ Practice/ideias/Quarto.js
@@ -0,0 +1,9 @@
+export class Quarto {
+    constructor(cama, armario, pc, mesa, cadeira) {
+        this.cama = cama;
+        this.armario = armario;
+        this.pc = pc;
+        this.mesa = mesa;
+        this.cadeira = cadeira;
+    };
+};
