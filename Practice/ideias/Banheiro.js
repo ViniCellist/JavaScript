70f3ac6a895896fa558f87a7c4038ec53fc4e69e
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-20 10:39:19 -0300

    Create Banheiro.js

---
+++ Practice/ideias/Banheiro.js
@@ -0,0 +1,8 @@
+export class Banheiro {
+    constructor(vaso, pia, box, chuveiro) {
+        this.vaso = vaso;
+        this.pia = pia;
+        this.box = box;
+        this.chuveiro = chuveiro;
+    }
+}
