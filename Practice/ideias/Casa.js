dd0fc7792d4d72255c01b7abed4458b118d99e63
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-20 10:39:21 -0300

    Create Casa.js

---
+++ Practice/ideias/Casa.js
@@ -0,0 +1,8 @@
+export class Casa {
+    constructor(sala, quarto, banheiro, cozinha) {
+        this.sala = sala;
+        this.quarto = quarto;
+        this.banheiro = banheiro;
+        this.cozinha = cozinha;
+    };
+};
