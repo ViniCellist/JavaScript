49897e396a42212606bfca8411f471dcc82dc41b
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-20 10:39:31 -0300

    Create Sala.js

---
+++ Practice/ideias/Sala.js
@@ -0,0 +1,9 @@
+export class Sala {
+    constructor(tv, sofa, poltrona, mesaCentro, tapete) {
+        this.tv = tv;
+        this.sofa = sofa;
+        this.poltrona = poltrona;
+        this.mesaCentro = mesaCentro;
+        this.tapete = tapete;
+    };
+};
