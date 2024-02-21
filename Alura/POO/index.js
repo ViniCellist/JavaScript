a4840ffa3207d303785dcdacfc66fa310f4ac3a0
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-21 15:59:03 -0300

    Update index.js

--- Alura/POO/index.js
+++ Alura/POO/index.js
@@ -1,15 +1,22 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Fulano", 11122233309);
 const contaC = new ContaCorrente(1001, cliente1);
 const contaP = new ContaPoupanca(1000, 1001, cliente1);
-
+const contaS = new ContaSalario(cliente1);
 
 
 /*testes*/
+contaS.depositar(100);
+contaS.sacar(10);
 
-console.error(contaC, contaP);
+
+
+/*saida*/
+
+console.error(contaS);
 
 
