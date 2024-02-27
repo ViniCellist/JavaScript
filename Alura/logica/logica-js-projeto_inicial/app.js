e7b8fe0be6811b30fdf8bf727a111c5dbda5fdf4
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-27 14:56:33 -0300

    Update app.js

--- Alura/logica/logica-js-projeto_inicial/app.js
+++ Alura/logica/logica-js-projeto_inicial/app.js
@@ -5,9 +5,9 @@ let numeroSecreto = 5;
 let chute = prompt("Escolha um número entre 1 e 10");
 
 if(chute == numeroSecreto) {
-    console.log('Acertou!');
+    console.log(`Acertou! O número secreto era ${numeroSecreto}!`);
 } else if (chute > numeroSecreto) {
-    console.log('o número secreto é menor!')
+    console.log(`o número secreto é menor que ${chute}!`);
 } else {
-    console.log('o número secreto é maior')
-}
+    console.log(`o número secreto é maior que ${chute}!`);
+};
