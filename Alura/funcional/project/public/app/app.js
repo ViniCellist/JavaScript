35539ae6e9b83eac14b5648350fedac7805bdbdc
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-27 16:17:30 -0300

    feat: countdown timer for clicks

--- Alura/funcional/project/public/app/app.js
+++ Alura/funcional/project/public/app/app.js
@@ -1,15 +1,17 @@
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { debounceTime, takeUntil } from './utils/operators.js';
 
-const operation = takeUntil(3, () =>
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log)
+const action = debounceTime(500, 
+    takeUntil(3, () => 
+        service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log) 
+    )
 );
 
 document
 .querySelector("#myButton")
-.onclick = operation;
+.onclick = action;
     
