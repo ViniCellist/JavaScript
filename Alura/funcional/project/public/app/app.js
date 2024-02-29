7e24b6d25f982330d3be57c2d6c0651df031a1cd
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-28 23:38:43 -0300

    Update app.js

--- Alura/funcional/project/public/app/app.js
+++ Alura/funcional/project/public/app/app.js
@@ -1,3 +1,4 @@
+import { timeoutPromise, log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { debounceTime, takeUntil, partialize, pipe } from './utils/operators.js';
@@ -8,8 +9,7 @@ const operations = pipe(
 );
 
 const action = operations(() => 
-    service
-    .sumItems('2143')
+    timeoutPromise(200, service.sumItems('2143'))
     .then(console.log)
     .catch(console.log) 
 );
