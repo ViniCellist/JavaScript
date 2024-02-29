a6d71972c161bdb3975337f44e28ba787dff1915
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-28 23:39:01 -0300

    feat: bad network error

--- Alura/funcional/project/public/app/utils/promise-helpers.js
+++ Alura/funcional/project/public/app/utils/promise-helpers.js
@@ -5,3 +5,9 @@ export const log = param => {
     console.log(param);
     return param;
 };
+
+export const timeoutPromise = (miliseconds, promise) => {
+    const timeout = new Promise((resolve, reject) => setTimeout(() => reject(`Limite da promise excedido (limite: ${miliseconds} ms)`), miliseconds));
+
+    return Promise.race([timeout, promise]);
+};
