04ee69ab70340c54657217c31eed2bc031cef396
Vinicius Duarte <vsduartestudy@outlook.com>
2024-02-29 12:51:07 -0300

    Update app.js

--- Alura/logica/logica-js-projeto_inicial/app.js
+++ Alura/logica/logica-js-projeto_inicial/app.js
@@ -1,16 +1,16 @@
 alert("Boas vindas ao Jogo do número secreto!");
 
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 
 let chute;
 let tries = 1;
 
 while(chute != numeroSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
-
+    chute = prompt("Escolha um número entre 1 e 100");
     if(chute == numeroSecreto) {
         alert(`Acertou! O número secreto era ${numeroSecreto}!`);
         alert(`Tentativas: ${tries}`)
+        break;
     } else if (chute > numeroSecreto) {
         alert(`o número secreto é menor que ${chute}!`);
     } else {
