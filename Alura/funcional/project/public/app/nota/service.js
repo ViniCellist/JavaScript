f4b65ba2d435679ca90d7230ae0195afd80b4ae2
Vinicius Duarte <vsduartestudy@outlook.com>
2024-03-01 23:14:34 -0300

    feat: monad

--- Alura/funcional/project/public/app/nota/service.js
+++ Alura/funcional/project/public/app/nota/service.js
@@ -1,14 +1,15 @@
 import { handleStatus } from "../utils/promise-helpers.js";
 import { partialize } from "../utils/operators.js";
 import { pipe, compose } from "../utils/operators.js";
+import { Maybe } from "../utils/maybe.js";
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(nota => nota.itens));
 
-const filterItemByCode = (code, items) => items.filter(item => item.codigo == code);
+const filterItemByCode = (code, itemsM) => itemsM.map(items => items.filter(item => item.codigo == code));
 
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const sumItemsValue = itemsM => itemsM.map(items => items.reduce((total, item) => total + item.valor, 0));
 
 
 
@@ -16,6 +17,7 @@ export const notasService = {
     listAll() {
         return fetch(API)
         .then(handleStatus)
+        .then(notas => Maybe.of(notas))
         .catch(err => {
             console.log(err);
             return Promise.reject('Não foi possível obter as notas fiscais');
@@ -31,6 +33,7 @@ export const notasService = {
         );
 
         return this.listAll()
-            .then(sumItems);
+            .then(sumItems)
+            .then(result => result.getOrElse(0))
     }
 };
