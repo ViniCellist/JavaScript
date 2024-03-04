5a926250663cff65fe02f1167a0cd2a7a564c6a4
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-04 06:35:07 -0300

    starter pack

---
+++ Alura/logica2/app.js
@@ -0,0 +1,11 @@
+const title = document.querySelector('h1');
+const paragraph = document.querySelector('p');
+const chute = document.querySelector('#chute');
+const newG = document.querySelector('#reiniciar');
+
+title.innerHTML = 'Jogo do número secreto';
+paragraph.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verifyGuess() {
+    
+}
