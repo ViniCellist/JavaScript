5422fd2993c8f76f360aeaf79bbf6773d32b41af
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-05 06:26:38 -0300

    feat: random number generate

--- Alura/logica2/app.js
+++ Alura/logica2/app.js
@@ -1,11 +1,20 @@
 const title = document.querySelector('h1');
 const paragraph = document.querySelector('p');
-const chute = document.querySelector('#chute');
+const chuteBtn = document.querySelector('#chute');
 const newG = document.querySelector('#reiniciar');
+let secretNumber = generateSecretNumber();
 
 title.innerHTML = 'Jogo do número secreto';
 paragraph.innerHTML = 'Escolha um número entre 1 e 10';
 
+chuteBtn.addEventListener('click', verifyGuess);
+
 function verifyGuess() {
-    
-}
+    let chute = document.querySelector('input').value;
+    console.log(chute == secretNumber);
+};
+
+function generateSecretNumber() {
+    return parseInt(Math.random() * 10 + 1);
+};
+
