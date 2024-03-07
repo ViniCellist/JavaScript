7597c3eae604d3417e92c2fb905a418a07ba5ca3
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-06 22:01:10 -0300

    feat: tip function

--- Alura/logica2/app.js
+++ Alura/logica2/app.js
@@ -1,9 +1,8 @@
 const title = document.querySelector('h1');
-const paragraph = document.querySelector('p');
+const paragraph = document.querySelector('.texto__paragrafo');
+const tip = document.querySelector('.texto__msg');
 const chuteBtn = document.querySelector('#chute');
-const newG = document.querySelector('#reiniciar');
 let secretNumber = generateSecretNumber();
-
 title.innerHTML = 'Jogo do número secreto';
 paragraph.innerHTML = 'Escolha um número entre 1 e 10';
 
@@ -11,10 +10,24 @@ chuteBtn.addEventListener('click', verifyGuess);
 
 function verifyGuess() {
     let chute = document.querySelector('input').value;
-    console.log(chute == secretNumber);
+    if (chute == secretNumber) {
+        paragraph.innerText = "";
+        tip.innerText = "ACERTOU!!";
+        document.querySelector('#reiniciar').removeAttribute('disabled');
+    } else if (chute > secretNumber) {
+        tip.innerText = `O número secreto é menor que ${chute}!`;
+        clearField();
+    } else {
+        tip.innerText = `O número secreto é maior que ${chute}!`;
+        clearField();
+    };
 };
 
 function generateSecretNumber() {
     return parseInt(Math.random() * 10 + 1);
 };
 
+function clearField() {
+    chute = document.querySelector('input');
+    chute.value = '';
+};
