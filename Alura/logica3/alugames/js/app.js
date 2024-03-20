ab846bab9042c3e3ed4978bf91f53d98a427bfbf
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 16:19:29 -0300

    feat: img change

--- Alura/logica3/alugames/js/app.js
+++ Alura/logica3/alugames/js/app.js
@@ -1,17 +1,20 @@
 const btns = document.querySelectorAll('.dashboard__item__button');
 const img = document.querySelectorAll('.dashboard__item__img');
 
-btns.forEach((e) => {
-    e.addEventListener('click', changeStatus);
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        changeStatus(btn, img[index]);
+    });
 });
 
-function changeStatus() {
-    if(this.classList.contains('dashboard__item__button--return')) {
-        this.classList.remove('dashboard__item__button--return');
-        this.innerHTML = `Alugar`;
+function changeStatus(btn, img) {
+    if(btn.classList.contains('dashboard__item__button--return')) {
+        btn.classList.remove('dashboard__item__button--return');
+        btn.innerHTML = `Alugar`;
+        img.classList.remove('dashboard__item__img--rented');
     } else {
-        this.classList.add('dashboard__item__button--return');
-        this.innerHTML = `Devolver`;
+        btn.classList.add('dashboard__item__button--return');
+        btn.innerHTML = `Devolver`;
+        img.classList.add('dashboard__item__img--rented');
     };
-
 };
